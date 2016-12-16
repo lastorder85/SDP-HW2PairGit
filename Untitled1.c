3bded4b816dc934cba3b83d29b4c24b9db0d50a5
Ittichote <wi.ittichote_st@tni.ac.th>
2016-12-16 03:13:30 +0000

    pushv2

--- Untitled1.c
+++ Untitled1.c
@@ -5,15 +5,24 @@
 
 int main(){
 	srand(time(NULL));
-
+	int i;
+	
 	int randomNumber1 = rand() % 10; //0-10
 	int randomNumber2 = rand() % 10; //0-10
 	int randomNumber3 = rand() % 10; //0-10
 		int randomNumber4 = rand() % 10; //0-10
 			int randomNumber5 = rand() % 10; //0-10
-				int randomNumber6 = rand() % 10; //0-10
+		int randomNumber6 = rand() % 10; //0-10
 	
-	printf("Lotto  is %d%d%d%d%d%d\n", randomNumber1,randomNumber2,randomNumber3,randomNumber4,randomNumber5,randomNumber6);
-
+	printf("Lotto  is Rank1 : %d%d%d%d%d%d\n", randomNumber1,randomNumber2,randomNumber3,randomNumber4,randomNumber5,randomNumber6);
+	printf("Lotto  last2Number : %d%d\n",randomNumber5,randomNumber6);
+	for(i=0;i<3;++i)
+	{
+	int randomNumber1 = rand() % 10; //0-10
+	int randomNumber2 = rand() % 10; //0-10
+	int randomNumber3 = rand() % 10; //0-10
+	printf("Lotto  is rank3 : %d%d%d\n", randomNumber1,randomNumber2,randomNumber3);
+	}
+	
 
 }
