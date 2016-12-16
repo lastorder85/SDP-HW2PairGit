876668e71e03aeef028ec4a374d57d7776c1c900
Naphattra Chaiyayangkul <ch.naphattra_st@tni.ac.th>
2016-12-16 03:01:46 +0000

    push v1

--- Untitled1.c
+++ Untitled1.c
@@ -1,4 +1,19 @@
-#include <stdio.h?
-main(){
-printf("Hello world");
+#include "stdio.h"
+#include "stdlib.h"
+
+#include "time.h"
+
+int main(){
+	srand(time(NULL));
+
+	int randomNumber1 = rand() % 10; //0-10
+	int randomNumber2 = rand() % 10; //0-10
+	int randomNumber3 = rand() % 10; //0-10
+		int randomNumber4 = rand() % 10; //0-10
+			int randomNumber5 = rand() % 10; //0-10
+				int randomNumber6 = rand() % 10; //0-10
+	
+	printf("Lotto  is %d%d%d%d%d%d\n", randomNumber1,randomNumber2,randomNumber3,randomNumber4,randomNumber5,randomNumber6);
+
+
 }
