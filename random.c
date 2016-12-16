d67827959b901864603b5369ea1c1d98f21e457e
Naphattra Chaiyayangkul <ch.naphattra_st@tni.ac.th>
2016-12-16 03:42:37 +0000

    end

--- random.c
+++ random.c
@@ -10,11 +10,21 @@ int main(){
 	int randomNumber3 = rand() % 10; //0-10
     int randomNumber4 = rand() % 10; //0-10
 	int randomNumber5 = rand() % 10; //0-10
-	int randomNumber6 = 9;
+	int randomNumber6 = rand() % 10; //0-10
 	
 	printf("Lotto is Rank1 : %d%d%d%d%d%d\n", randomNumber1,randomNumber2,randomNumber3,randomNumber4,randomNumber5,randomNumber6);
 	
+	for(i=0;i<4;++i)
+	{
+	int randomNumber1 = rand() % 10; //0-10
+	int randomNumber2 = rand() % 10; //0-10
+	int randomNumber3 = rand() % 10; //0-10
+	printf("Lotto is Rank3 : %d%d%d\n", randomNumber1,randomNumber2,randomNumber3);
+	}
 	
+	int randomNumber7 = rand() % 10; //0-10
+	int randomNumber8 = rand() % 10; //0-10
+	printf("Lotto Last 2 Number : %d%d\n",randomNumber7,randomNumber8);
 	
 
 }
